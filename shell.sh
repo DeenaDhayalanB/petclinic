3989f4486da2414df31a254ff2868493f13663f8
DeenaDhayalanB <31236522+DeenaDhayalanB@users.noreply.github.com>
2017-11-22 15:18:26 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -12,8 +12,13 @@ cp -r source/* dest/
 cd dest
 mvn clean package
 
+cp -r target/ ../
+rm -rf *
+cp -r ../target .
+
 git config --global user.email "DeenaDhayalan.B@cognizant.com"
 git config --global user.name "DeenaDhayalanB"
 
 git add .
 git commit -m "packaged output"
+git push -u DeenaDhayalanB:Deepak123

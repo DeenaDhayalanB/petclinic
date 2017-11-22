d169861abe4f055b1704039d14ace0b695ca0f38
DeenaDhayalanB <31236522+DeenaDhayalanB@users.noreply.github.com>
2017-11-22 14:51:27 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -8,5 +8,8 @@ git clone resource-tutorial updated-gist
 cd updated-gist
 mvn clean package
 
+git config --global user.email "DeenaDhayalan.B@cognizant.com"
+git config --global user.name "DeenaDhayalanB"
+
 git add .
 git commit -m "packaged output"
